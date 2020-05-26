6b2c97beb1c2540ff9f8dd5adb62b1f2ed50a840
Yuri Pedroso <yurihp01@hotmail.com>
2020-05-26 11:26:12 -0300

    Initial Commit

---
+++ desafio-ios-yuri-pedrosoTests/desafio_ios_yuri_pedrosoTests.swift
@@ -0,0 +1,34 @@
+//
+//  desafio_ios_yuri_pedrosoTests.swift
+//  desafio-ios-yuri-pedrosoTests
+//
+//  Created by Yuri on 26/05/20.
+//  Copyright © 2020 DevVenture. All rights reserved.
+//
+
+import XCTest
+@testable import desafio_ios_yuri_pedroso
+
+class desafio_ios_yuri_pedrosoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
